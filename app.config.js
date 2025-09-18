44cc0a0b0cc0a7f9cc15d2b563c7363c99e38716
tadashi <tahofu89@gmail.com>
2025-09-18 13:19:31 +0900

    異なる開発環境のバージョンを追加しました

---
+++ app.config.js
@@ -0,0 +1,41 @@
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+
+const getUniqueIdentifier = () => {
+    if (IS_DEV) {
+        return 'com.nt19990508.ShokuMeter.dev';
+    }
+
+    if (IS_PREVIEW) {
+        return 'com.nt19990508.ShokuMeter.preview';
+    }
+
+    return 'com.nt19990508.ShokuMeter';
+};
+
+const getAppName = () => {
+    if (IS_DEV) {
+        return '食メーター (Dev)';
+    }
+
+    if (IS_PREVIEW) {
+        return '食メーター (Preview)';
+    }
+
+    return 'ShokuMeter';
+};
+
+
+export default ({ config }) => ({
+    ...config,
+    name: getAppName(),
+    ios: {
+        ...config.ios,
+        bundleIdentifier: getUniqueIdentifier(),
+    },
+    android: {
+        ...config.android,
+        package: getUniqueIdentifier(),
+    },
+});
+
