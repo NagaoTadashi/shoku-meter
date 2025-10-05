dac07e7396e4fcfd909e7dd4b1ce8cdea42cbbba
tadashi <tahofu89@gmail.com>
2025-10-05 12:36:41 +0900

    app configを修正しました

--- app.config.js
+++ app.config.js
@@ -1,41 +1,41 @@
-// const IS_DEV = process.env.APP_VARIANT === 'development';
-// const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
 
-// const getUniqueIdentifier = () => {
-//     if (IS_DEV) {
-//         return 'com.nt19990508.ShokuMeter.dev';
-//     }
+const getUniqueIdentifier = () => {
+    // if (IS_DEV) {
+    //     return 'com.nt19990508.ShokuMeter.dev';
+    // }
 
-//     if (IS_PREVIEW) {
-//         return 'com.nt19990508.ShokuMeter.preview';
-//     }
+    // if (IS_PREVIEW) {
+    //     return 'com.nt19990508.ShokuMeter.preview';
+    // }
 
-//     return 'com.nt19990508.ShokuMeter';
-// };
+    return 'com.nt19990508.ShokuMeter';
+};
 
-// const getAppName = () => {
-//     if (IS_DEV) {
-//         return '食メーター (Dev)';
-//     }
+const getAppName = () => {
+    // if (IS_DEV) {
+    //     return '食メーター (Dev)';
+    // }
 
-//     if (IS_PREVIEW) {
-//         return '食メーター (Preview)';
-//     }
+    // if (IS_PREVIEW) {
+    //     return '食メーター (Preview)';
+    // }
 
-//     return 'ShokuMeter';
-// };
+    return 'ShokuMeter';
+};
 
 
-// export default ({ config }) => ({
-//     ...config,
-//     name: getAppName(),
-//     ios: {
-//         ...config.ios,
-//         bundleIdentifier: getUniqueIdentifier(),
-//     },
-//     android: {
-//         ...config.android,
-//         package: getUniqueIdentifier(),
-//     },
-// });
+export default ({ config }) => ({
+    ...config,
+    name: getAppName(),
+    ios: {
+        ...config.ios,
+        bundleIdentifier: getUniqueIdentifier(),
+    },
+    android: {
+        ...config.android,
+        package: getUniqueIdentifier(),
+    },
+});
 
